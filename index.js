a724f2d24ae17ac938273e29af477bfa00ca693a
Gerard Banasig <tuxrace@yahoo.com>
2023-07-02 14:54:27 +0800

    feature(main): update script

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function writeReadMe() {
 		const { path, sha, content, encoding } = res.data;
     console.log(res.data)
 		const rawContent = Buffer.from(content, encoding).toString();
-		const startIndex = rawContent.indexOf("## Other Projects");
+		const startIndex = rawContent.indexOf("## Date");
 		const updatedContent = `${startIndex === -1 ? rawContent : rawContent.slice(0, startIndex)}\n${getNewTemplateSection()}`;
 		writeNewReadme(path, sha, encoding, updatedContent);
 	} catch (error) {
--- template.md
+++ template.md
@@ -1,1 +1,1 @@
-news
+new date
